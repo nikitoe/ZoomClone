b04388602da352859cf0f83178a23be847a795f6
Jiyong Sim <sjy19910222@gmail.com>
2021-11-17 13:32:31 +0900

    #0.4 Recap

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ app.set("views", __dirname + "/views");
 //public url을 생성해서 유저에게 파일을 공유
 app.use("/public",express.static(__dirname + "/public"));
 
+//home.pug를 render해주는 route handler를 만듬
 app.get("/", (req, res) => res.render("home"));
+
+//유저가 어떤 url로 이동하든지 홈으로 돌려보내기
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
