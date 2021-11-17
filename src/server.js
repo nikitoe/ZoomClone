40c7a571f866c4407bfb1eb2841f5d64972258b4
Jiyong Sim <sjy19910222@gmail.com>
2021-11-17 13:13:30 +0900

    #0.3 Frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ import express from "express";
 
 const app = express();
 
-app.set("veiw engine", "pug");
+//pug로 view engine 설정
+app.set("view engine", "pug");
+
+//express에 template가 어디 있는지 설정
 app.set("views", __dirname + "/views");
+
+//static 작업: 유저가 /public으로 가게 되면 __dirname + "/public"폴더를 보여주게 함
+//public url을 생성해서 유저에게 파일을 공유
+app.use("/public",express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+app.listen(3000, handleListen);  
