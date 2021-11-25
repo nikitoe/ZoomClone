be8a241ab156ec6ca50e55559c3dd0cae52107f6
Jiyong Sim <sjy19910222@gmail.com>
2021-11-25 13:02:21 +0900

    #3.0 User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,46 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia () {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true,
+        });
+        myFace.srcObject = myStream;
+    } catch(e) {
+        console.log(e);
+    };
+};
+
+getMedia();
+
+function handleMuteBtn () {
+    if (!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    };
+};
+
+function handleCameraBtn () {
+    if (cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    };
+};
+
+muteBtn.addEventListener("click", handleMuteBtn);
+cameraBtn.addEventListener("click", handleCameraBtn);
