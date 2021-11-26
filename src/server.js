66fdea2c217011fa7934593a46e74db975edb224
Jiyong Sim <sjy19910222@gmail.com>
2021-11-26 13:19:59 +0900

    #Change Setting

--- src/server.js
+++ src/server.js
@@ -3,28 +3,28 @@ import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
 
-const app = express();
+const appVideo = express();
 
 //pug로 view engine 설정
-app.set("view engine", "pug");
+appVideo.set("view engine", "pug");
 
 //express에 template가 어디 있는지 설정
-app.set("views", __dirname + "/views");
+appVideo.set("views", __dirname + "/views");
 
 //static 작업: 유저가 /public으로 가게 되면 __dirname + "/public"폴더를 보여주게 함
 //public url을 생성해서 유저에게 파일을 공유
-app.use("/public",express.static(__dirname + "/public"));
+appVideo.use("/public",express.static(__dirname + "/public"));
 
 //home.pug를 render해주는 route handler를 만듬
-app.get("/", (req, res) => res.render("home"));
+appVideo.get("/", (req, res) => res.render("home"));
 
 //유저가 어떤 url로 이동하든지 홈으로 돌려보내기
-app.get("/*", (req, res) => res.redirect("/"));
+appVideo.get("/*", (req, res) => res.redirect("/"));
 
 //나의 http 서버를 만듬
-const server = http.createServer(app);
+const server = http.createServer(appVideo);
 const swServer = SocketIO(server);
 
-//app.listen(3000, handleListen);  
+//appVideo.listen(3000, handleListen);  
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
